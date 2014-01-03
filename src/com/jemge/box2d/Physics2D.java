a81d999345f9ca1f9d5ca14f992b23a760a8d67c
mrbarsack <mrbarsack@gmail.com>
2014-01-03 12:34:03 +0100

    2014 :-)

--- src/com/jemge/box2d/Physics2D.java
+++ src/com/jemge/box2d/Physics2D.java
@@ -1,5 +1,5 @@
 /*
- * Copyright [2013] @author file
+ * Copyright [2014] @author file
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
